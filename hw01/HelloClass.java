b200c49818fd979b86e5ce91f660e937e2c95f29
skenney518 <spk318@lehigh.edu>
2014-08-29 19:25:24 +0000

    updated HelloClass

---
+++ hw01/HelloClass.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////
+//Shaun Kenney
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+// define a class
+public class HelloClass{
+    
+// add main method
+ public static void main(String[] args) {
+     
+//  print the Welcome Message
+    System.out.println("   -----------      ");
+    System.out.println("   | WELCOME |"      );
+    System.out.println("   -----------"      );
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-S--P--K--3--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  V  V  V  V  V  V  ");
+    System.out.println("I went to Spain over Spring Break.");
+    
+    //test/testtest
+ }
+}
+
+
